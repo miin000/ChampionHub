332a60d7fccf73246ef87ec342f5ace9d44c3950
miin0 <phammin000@gmail.com>
2024-12-19 13:25:54 +0700

    test routing in laravel

---
+++ FlashNoterWebApp/flashNoterWebApp/resources/views/user/user.blade.php
@@ -0,0 +1,3 @@
+<div>
+   dsadsadsadsdd
+</div>
--- FlashNoterWebApp/flashNoterWebApp/routes/web.php
+++ FlashNoterWebApp/flashNoterWebApp/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    return view('user.user'); // Gọi file resources/views/user/user.blade.php
+});
